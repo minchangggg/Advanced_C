b5bd1c95313d3015aed58b21765acbdb20929d8a
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2023-12-16 18:38:52 +0700

    Update main.c

--- Lesson_2/Ex/main.c
+++ Lesson_2/Ex/main.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "controller.h"
 
 int main() {
     controlDevice(LIGHT, ON); 
-    controlDevice(AIR_CONDITIONER, SET_TEMPERATURE, 18);
+    controlDevice(AIR_CONDITIONER, ON, SET_TEMPERATURE, 18);
     controlDevice(FINGERPRINT_LOCK, UNLOCK); 
-    controlDevice(MUSIC_PLAYER, CHANGE_TRACK, "Price tag");
+    controlDevice(MUSIC_PLAYER, ON, CHANGE_TRACK,"Price tag");
     return 0;
 }
