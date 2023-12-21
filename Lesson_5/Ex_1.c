a17aac22c08a04e6cc490144328845b3f529883c
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2023-12-21 17:35:09 +0700

    Update Ex_1.c

--- Lesson_5/Ex_1.c
+++ Lesson_5/Ex_1.c
@@ -6,18 +6,18 @@ int exception_code;
 char *error_code;
 
 #define TRY if ((exception_code = setjmp(buf)) == 0)
-#define CATCH(x) else if (exception_code == (x)) {printf("%s\n", error_code);}
-#define THROW(x, ERROR_CODE) do {error_code = #ERROR_CODE; longjmp(buf, (x));} while(0) // oke
+#define CATCH(x) else if (exception_code == (x)) printf("%s\n", error_code);
+#define THROW(x, ERROR_CODE) {error_code = #ERROR_CODE; longjmp(buf, (x));}
 
-double divide(int a, int b) {
+float divide(int a, int b) {
     if (b == 0) THROW (1, Error: Divide by 0!); 
     if (a*b < 0) THROW (2, Error: Quotient is less than 0!);  
-    return (double) a/b;
+    return (float) a/b;
 }
 
 int main() {
-    int a = 31; int b = -12;
-    TRY {printf("Result is %.2f\n", divide(a, b));} 
+    int a = 31; int b = 12;
+    TRY printf("Result is %.2f\n", divide(a, b));
     CATCH (exception_code); 
     return 0;
 }
