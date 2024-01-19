50b81220b69061ac25263d59d8f134843c118067
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-01-19 22:29:32 +0700

    Create main.c

---
+++ Midterm exam/Answer/main.c
@@ -0,0 +1,11 @@
+#include "Menu.h";
+
+Member member;
+MemberNode* list = NULL;
+char* searchValue; char* uid;
+
+int main() {
+	Menu( "10000_Members.csv", &list, member, searchValue, uid);
+	system("pause");
+	return 0;
+}
