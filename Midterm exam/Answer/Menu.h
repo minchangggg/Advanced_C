8cae2b7f9e624830ad528eb327c47c2a034b017d
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-01-20 23:24:43 +0700

    Update Menu.h

--- Midterm exam/Answer/Menu.h
+++ Midterm exam/Answer/Menu.h
@@ -1,4 +1,12 @@
-#pragma once
+/*
+* File: Menu.h
+* Author: Ton Nu Minh Trang
+* Date: 15/01/2023
+* Description: This file is a menu header file that for program management 
+*/
+
+#ifndef __MENU_H
+#define __MENU_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,3 +17,5 @@
 void Menu(const char* filename, MemberNode* list, Member member, char* searchValue, char* uid);
 void readData(const char* filename, MemberNode* list);
 void writeData(const char* filename, MemberNode* list);
+
+#endif
