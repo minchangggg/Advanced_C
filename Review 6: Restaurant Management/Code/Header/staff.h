d80efb60ba24e49d957250d15ab2db8079ab8cc8
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-24 01:53:52 +0700

    Update staff.h

--- Review 6: Restaurant Management/Code/Header/staff.h
+++ Review 6: Restaurant Management/Code/Header/staff.h
@@ -1,1 +1,27 @@
+#ifndef _STAFF_H
+#define _STAFF_H
 
+#include <vector>
+#include "table.h"
+
+using namespace std;
+
+class Staff {
+    private:
+        vector<Table> tableList;
+    public:
+        Staff() {}
+        Staff (int _numTable) {
+            for (int i = 0; i < (_numTable); i++) {
+                Table newTable;
+                tableList.push_back(newTable);  
+            }
+        }
+    
+    void displayTable(); // hiển thị tất cả table đc set up 
+    bool isFull (); // kiểm tra có full bàn không 
+    void chooseTable (); // chọn bàn nếu còn dư hơn 1 bàn 
+    void menuStaff();
+};
+
+#endif
