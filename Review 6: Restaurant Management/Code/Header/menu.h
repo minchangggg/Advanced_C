d96bb2c667f3c58577e1385b89216cefb86dd1f3
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-24 01:55:00 +0700

    Update menu.h

--- Review 6: Restaurant Management/Code/Header/menu.h
+++ Review 6: Restaurant Management/Code/Header/menu.h
@@ -1,1 +1,28 @@
+#ifndef _MENU_H
+#define _MENU_H
 
+#include "manager.h"
+#include "staff.h"
+
+using namespace std;
+
+typedef enum {
+    managerWork,
+    staffWork
+} Title;
+
+typedef struct {
+    Manager managerData;
+    Staff staffaData;
+} Database;
+
+static int choice;
+static int quantity;
+
+void mainMenu (Database database);
+
+void menuStaff (Database database);
+
+void menuManager (Database database); 
+
+#endif
