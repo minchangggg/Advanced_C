0fb531de9e90abc5407f1e237ac0aba691284bdb
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-24 01:40:23 +0700

    Create manager.cpp

---
+++ Review 6: Restaurant Management/Code/Source/manager.cpp
@@ -0,0 +1,10 @@
+#include "manager.h"
+using namespace std;
+
+void Manager::setNumTable (int _numTable) {}
+int Manager::setNumTable () {}
+
+void Manager::addDish(Dish dish) {} 
+void Manager::editDish(Dish dish) {} 
+void Manager::deleteDish(Dish dish) {} 
+void Manager::showMenu() {} 
