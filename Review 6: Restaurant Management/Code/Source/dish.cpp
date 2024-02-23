34128bc8d058685c4f2dcedebf3d23e619a3a8cd
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-23 23:27:07 +0700

    Update dish.cpp

--- Review 6: Restaurant Management/Code/Source/dish.cpp
+++ Review 6: Restaurant Management/Code/Source/dish.cpp
@@ -1,1 +1,37 @@
+#include "dish.h"
+using namespace std;
 
+int Dish::getID() {
+    return ID;
+}
+
+void Dish::setName(string _name) {
+    name = _name;
+}
+string Dish::getName() {
+    return name;
+}
+
+void Dish::setPrice(int _price) {
+    price = _price;
+}
+
+int Dish::getPrice() {
+    return price;
+}
+
+void Dish::setData() {
+    cout << "\t\t\tEnter name of dish: ";
+    string _name; 
+    cin.ignore(); cin >> _name;
+    setName(_name);
+
+    cout << "\t\t\tEnter price of dish: ";
+    int _price; cin >> _price;
+    setPrice(_price);
+}
+
+void Dish::getData() {
+    cout << "ID" << "\t\t\tName" << "\t\t\tPrice" << endl;
+    cout << getID() << "\t\t\t" << getName() << "\t\t\t" << getPrice();
+}
