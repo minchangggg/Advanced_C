9ddfb996dda20d6fe69c62417ee7fc9b0f9f2222
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-23 23:06:34 +0700

    Update dish.h

--- Review 6: Restaurant Management/Header/dish.h
+++ Review 6: Restaurant Management/Header/dish.h
@@ -1,1 +1,40 @@
+#ifndef _DISH_H
+#define _DISH_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Dish {
+private:
+    short int ID;
+    string name;
+    int price;
+
+public:
+    Dish() {
+        this->ID = 0;
+        this->name = "";
+        this->price= 0;
+    }
+
+    void setID(short int _ID);
+    short int getID();
+
+    void setName(string _name);
+    string getName();
+
+    void setPrice(int _price);
+    int getPrice();
+
+    void setData();
+    void getData();
+};
+
+typedef struct {
+    int num;
+    Dish dish;
+} Order;
+
+#endif
