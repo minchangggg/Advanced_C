25e715511f1631e868b35e0eaa4332e3a3dcd1be
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-29 03:02:54 +0700

    Update manager.h

--- Review 7: Worker Management/Code/Header/manager.h
+++ Review 7: Worker Management/Code/Header/manager.h
@@ -1,1 +1,31 @@
+#ifndef _MANAGER_H
+#define _MANAGER_H
 
+#include "../Header/employee.h"
+#include "../Header/room.h"
+#include "../Header/service.h"
+
+typedef enum {
+    Employee_function,
+    Room_function,
+    Service_function
+} Function;
+
+typedef struct {
+    ManageEmployee employee;
+    ManageRoom room;
+    ManageService service;
+} Database;
+
+class Manager {
+private:
+    int password;
+public:
+    Manager() {
+        this->password = 123456789;
+    }
+    int getPassword();
+    void menuManager(Database &database);
+};
+
+#endif
