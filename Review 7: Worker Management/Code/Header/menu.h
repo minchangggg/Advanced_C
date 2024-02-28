6a6221c22300849b8e06c8d5ccb48f95a2c3a9e4
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-02-29 03:12:28 +0700

    Update menu.h

--- Review 7: Worker Management/Code/Header/menu.h
+++ Review 7: Worker Management/Code/Header/menu.h
@@ -1,28 +1,8 @@
 #ifndef _MENU_H
 #define _MENU_H
 
-#include "../Header/employee.h"
-#include "../Header/room.h"
-#include "../Header/service.h"
-
-typedef enum {
-    Employee_function,
-    Room_function,
-    Service_function
-} Function;
-
-typedef struct {
-    ManageEmployee employee;
-    ManageRoom room;
-    ManageService service;
-} Database;
-
-static int _choice;
-
-void mainMenu(Database &database);
+#include "../Header/manager.h"
 
 void menuManager(Database &database);
-
-void menuStaff(Database &database);
 
 #endif
