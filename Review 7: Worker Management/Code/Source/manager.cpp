bd9a5a630ff43c96a02ae76b48c42b30100b5188
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-01 03:27:46 +0700

    Update manager.cpp

--- Review 7: Worker Management/Code/Source/manager.cpp
+++ Review 7: Worker Management/Code/Source/manager.cpp
@@ -1,17 +1,27 @@
 #include "../Header/manager.h"
 using namespace std; 
 
+int Manager::getAccount() {
+    return account;
+}
+
 int Manager::getPassword() {
     return password;
 }
 
 void Manager::menuManager(Database &database) {
 menuManager_start:
-    int _password = 0; int _choice = 0; 
-    cout << "\t\t\tPlease Enter Password To Access System: ";
+    int _password = 0; int _account = 0; int _choice = 0; 
+
+    cout << "\t\t\t\t\t\tLOG IN" << endl;
+
+    cout << "\t\t\tPlease Enter Account / Phone number: ";
+    cin >> _account;
+ 
+    cout << "\t\t\tPlease Enter Password: ";
     cin >> _password;
 
-    if (getPassword() != _password) {
+    if ((getAccount() != _account) || (getPassword() != _password)) {
         cout << "\t\t\t The Password Is Incorrect!" << endl << endl;
 
         cout << "\t\t\t 1. Re-enter The Password" << endl;
@@ -39,14 +49,14 @@ menuManager_start:
         }
     }
 
-    else {
+    else if((getAccount() == _account) && (getPassword() == _password)) {
         Function function;
         do {
             do {
                 system("cls");
 
                 cout << "\t\t\t-----------------------------" << endl;
-                cout << "\t\t\t| STAFF MANAGEMENT SYSTEM |" << endl;
+                cout << "\t\t\t|       MANAGER WORKING     |" << endl;
                 cout << "\t\t\t-----------------------------" << endl;
                 cout << "\t\t\t 1. Employee Management" << endl; // 1. Quản lý nhân viên
                 cout << "\t\t\t 2. Room Management" << endl;  // 2. Quản lý phòng
