640183d9f5987a101f2ac266b583ff739249a8e8
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-03 14:58:12 +0700

    Update employee.cpp

--- Review 7: Worker Management/Code/Source/employee.cpp
+++ Review 7: Worker Management/Code/Source/employee.cpp
@@ -1,40 +1,108 @@
+/*
+* File: employee.h
+* Author: Ton Nu Minh Trang
+* Date: 15/02/2024
+* Description: This file contains all the functions/methods to manage employees of hotel
+*/
+
 #include "../Header/employee.h"
 using namespace std;
 
-// class Employee _______________________________________________________________________________________________
-
+/*
+* Class: Employee
+* Function: setID
+* Discription: This function is used for setting ID of employee
+* Input:   _ID (ID of employee)
+* Output:  return: None
+*/
 void Employee::setID (int _ID) {
     ID = _ID;
 }
 
+/*
+* Class: Employee
+* Function: getID
+* Discription: This function is used for getting ID of employee
+* Input:   None
+* Output:  return: ID (ID of employee)
+*/
 int Employee::getID() {
     return ID;
 }
 
+/*
+* Class: Employee
+* Function: setName
+* Discription: This function is used for setting name of employee
+* Input:   _name (name of employee)
+* Output:  return: None
+*/
 void Employee::setName(string _name) {
     name = _name;
 }
 
+/*
+* Class: Employee
+* Function: getName
+* Discription: This function is used for getting name of employee
+* Input:   None
+* Output:  return: name (name of employee)
+*/
 string Employee::getName() {
     return name;
 }
 
+/*
+* Class: Employee
+* Function: setTitle
+* Discription: This function is used for setting title of employee
+* Input:   _title (title of employee)
+* Output:  return: None
+*/
 void Employee::setTitle(string _title) {
     title = _title;
 }
 
+/*
+* Class: Employee
+* Function: getTitle
+* Discription: This function is used for getting title of employee
+* Input:   None
+* Output:  return: title (title of employee)
+*/
 string Employee::getTitle() {
     return title;
 }
 
+/*
+* Class: Employee
+* Function: setPhone
+* Discription: This function is used for setting phone number of employee
+* Input:   _phone (phone number of employee)
+* Output:  return: None
+*/
 void Employee::setPhone(int _phone) {
     phone = _phone;
 }
 
+/*
+* Class: Employee
+* Function: getPhone
+* Discription: This function is used for getting phone number of employee
+* Input:   None 
+* Output:  return: phone (phone number of employee)
+*/
 int Employee::getPhone() {
     return phone;
 }
 
+/*
+* Class: Employee
+* Function: setData
+* Discription: This function is used for setting data of employee
+* Input:   None
+* Output:  return: None
+*/
 void Employee::setData() {
     cout << "\t\t\tEnter Name: ";
     string _name; 
@@ -53,16 +121,35 @@ void Employee::setData() {
     setTitle(_title);
 }
 
+/*
+* Class: Employee
+* Function: setShift
+* Discription: This function is used for setting shift of employee
+* Input:   _shift (_shift of employee)
+* Output:  return: None
+*/
 void Employee::setShift(Shift _shift) {
     shift = _shift;
 }
 
+/*
+* Class: Employee
+* Function: getShift
+* Discription: This function is used for getting shift of employee
+* Input:   None 
+* Output:  return: shift (shift of employee)
+*/
 Shift Employee::getShift() {
     return shift;
 }
 
-// class ManageEmployee _______________________________________________________________________________________________
-
+/*
+* Class: ManageEmployee
+* Function: addEmployee
+* Discription: This Function is used for adding a new employee
+* Input:   None
+* Output:  return: None
+*/
 void ManageEmployee::addEmployee() {
     Employee newEmployee;
     newEmployee.setData();
@@ -70,6 +157,13 @@ void ManageEmployee::addEmployee() {
     employeeList.push_back(newEmployee);
 } 
 
+/*
+* Class: ManageEmployee
+* Function: editEmployee
+* Discription: This Function is used for editing details of employee
+* Input:   _ID (ID of employee)
+* Output:  return: None
+*/
 void ManageEmployee::editEmployee(int _ID) {
     list<Employee>::iterator it;
     for (it = employeeList.begin(); it != employeeList.end(); ++it) {
@@ -83,6 +177,13 @@ void ManageEmployee::editEmployee(int _ID) {
     }
 } 
 
+/*
+* Class: ManageEmployee
+* Function: deleteEmployee
+* Discription: This Function is used for deleting details of employee
+* Input:   _ID (ID of employee)
+* Output:  return: None
+*/
 void ManageEmployee::deleteEmployee(int _ID) {
     list<Employee>::iterator it;
     for (it = employeeList.begin(); it != employeeList.end(); ++it) {
@@ -92,6 +193,14 @@ void ManageEmployee::deleteEmployee(int _ID) {
     }
 } 
 
+/*
+* Class: ManageEmployee
+* Function: setTimePerDay
+* Discription: This function is used for setting shift per day (employee can choose up to 2 shifts in 1 day)
+* Input:   time[0]: time for 1st shift in day
+           time[1]: time for 2nd shift in day 
+* Output:  return: None
+*/
 void ManageEmployee::setTimePerDay(Time day[2]) {
     int choiceTime = 0;
 
@@ -146,6 +255,13 @@ void ManageEmployee::setTimePerDay(Time day[2]) {
     }
 }
 
+/*
+* Class: ManageEmployee
+* Function: setShift
+* Discription: This function is used for setting weekly shift timetable
+* Input:   _ID (ID of employee)
+* Output:  return: None
+*/
 void ManageEmployee::setShift(int _ID) {
     list<Employee>::iterator it;
     for (it = employeeList.begin(); it != employeeList.end(); ++it) {
@@ -172,6 +288,13 @@ void ManageEmployee::setShift(int _ID) {
     }  
 } 
 
+/*
+* Class: ManageEmployee
+* Function: showEmployeeList
+* Discription: This function is used for displaying employees of hotel
+* Input:   None
+* Output:  return: None
+*/
 void ManageEmployee::showEmployeeList() {
     int count = 0;
     cout << "\tNo" << "\t\tID" << "\t\tName" << "\t\tPhone Number" << "\t\tTitle" << endl;
@@ -183,6 +306,13 @@ void ManageEmployee::showEmployeeList() {
     }
 } 
 
+/*
+* Class: ManageEmployee
+* Function: tableShift
+* Discription: This function is used for displaying weekly shift timetable
+* Input:   None
+* Output:  return: None
+*/
 void ManageEmployee::tableShift() {
     cout << "\tName" << "\t\tMon" << "\t\tTue" << "\t\tWed" << "\t\tThur" << "\t\tFri" << endl;
     cout << "______________________________________________________________________________________________________________________________________________" << endl;
@@ -194,6 +324,13 @@ void ManageEmployee::tableShift() {
     }
 } 
 
+/*
+* Class: ManageEmployee
+* Function: menuManageEmployee
+* Discription: This function is used for managing employees of hotel
+* Input:   None
+* Output:  return: None
+*/
 void ManageEmployee::menuManageEmployee() {
 menuManageEmployee_start:
     int _choice = 0;
@@ -201,22 +338,23 @@ menuManageEmployee_start:
     cout << "\t\t\t|     EMPLOYEE MANAGEMENT     |" << endl;
     cout << "\t\t\t-------------------------------" << endl;
     do { 
-        cout << "\t\t\t 1. Add New Employee Details" << endl; // 1. Thêm nhân viên
-        cout << "\t\t\t 2. Modify Employee Detail" << endl; // 2. Sửa thông tin nhân viên
-        cout << "\t\t\t 3. Delete Employee Detail" << endl; // 3. Xóa nhân viên
-        cout << "\t\t\t 4. Set Weekly Shift For Employee" << endl; // 4. Set ca làm cho nhân viên
-        cout << "\t\t\t 5. Employee List" << endl; // 5. Danh sách thông tin nhân viên
-        cout << "\t\t\t 6. Weekly Shift Timetable " << endl; // 6. Thời gian làm việc
-        cout << "\t\t\t 7. Turn Back Manager Menu " << endl; // 7. Quay lại main menu
-        cout << "\t\t\t 8. Exit" << endl; // 8. Thoát
+        cout << "\t\t\t 1. Add New Employee Details" << endl; 
+        cout << "\t\t\t 2. Modify Employee Detail" << endl; 
+        cout << "\t\t\t 3. Delete Employee Detail" << endl; 
+        cout << "\t\t\t 4. Set Weekly Shift For Employee" << endl; 
+        cout << "\t\t\t 5. Display Employee List" << endl; 
+        cout << "\t\t\t 6. Weekly Shift Timetable" << endl; 
+        cout << "\t\t\t 7. Turn Back Manager Menu" << endl; 
+        cout << "\t\t\t 8. Exit" << endl;
         cout << "\t\t\t............................" << endl;
         cout << "\t\t\tPlease Enter Your Choice: ";
         cin >> _choice;
     } while (_choice < 1 || _choice > 8);
 
     switch (_choice) {
-        case 1: // 1. Thêm nhân viên
-        add_employee:
+        case 1: // 1. Add New Employee Details
+        {
+            add_employee:
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "------------------------------------ Add New Employee Details -----------------------------------------" << endl;
             addEmployee();
@@ -233,8 +371,10 @@ menuManageEmployee_start:
             else goto menuManageEmployee_start;
 
             break;
+        }
 
-        case 2: // 2. Sửa thông tin nhân viên
+        case 2: // 2. Modify Employee Detail
+        {
         modify_employee:
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "------------------------------------- Modify Employee Detail ------------------------------------------" << endl;
@@ -254,8 +394,10 @@ menuManageEmployee_start:
             else goto menuManageEmployee_start;
 
             break;
+        }
 
-        case 3: // 3. Xóa nhân viên
+        case 3: // 3. Delete Employee Detail
+        {
         delete_employee:
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "------------------------------------- Delete Employee Detail ------------------------------------------" << endl;            
@@ -275,8 +417,10 @@ menuManageEmployee_start:
             else goto menuManageEmployee_start;
 
             break;
+        }
         
-        case 4: // 4. Set ca làm cho nhân viên
+        case 4: // 4. Set Weekly Shift For Employee
+        {
         set_weekly_shift:
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "--------------------------------- Set Weekly Shift For Employee----------------------------------------" << endl;            
@@ -296,8 +440,10 @@ menuManageEmployee_start:
             else goto menuManageEmployee_start;
 
             break;
+        }
 
-        case 5: // 5. Danh sách thông tin nhân viên
+        case 5: // 5. Display Employee List
+        {
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "--------------------------------------- Employee List -------------------------------------------------" << endl;
             showEmployeeList();
@@ -312,8 +458,9 @@ menuManageEmployee_start:
             goto menuManageEmployee_start;
 
             break;
-        
-        case 6: // 6. Thời gian làm việc
+        }
+        case 6: // 6. Weekly Shift Timetable
+        {
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "------------------------------------Weekly Shift Timetable --------------------------------------------" << endl;    
             tableShift();
@@ -325,12 +472,15 @@ menuManageEmployee_start:
                 cin >> _choice;
             } while (_choice != 1);
 
-            goto menuManageEmployee_start;           
+            goto menuManageEmployee_start;     
 
-        case 7: // 7. Quay lại Manager menu
+            break;
+        }      
+
+        case 7: // 7. Turn Back Manager Menu
             break;
 
-        case 8: // 8. Thoát
+        case 8: // 8. Exit
             cout << "\n-------------------------------------------------------------------------------------------------------" << endl;
             cout << "--------------------------------------- Program Is Exit -----------------------------------------------" << endl;
             exit(0);
