742f9f6f3c653f4938653fd171b18fdad6f38e82
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-04 03:34:00 +0700

    Update dish.cpp

--- Review 6: Restaurant Management/Code/Source/dish.cpp
+++ Review 6: Restaurant Management/Code/Source/dish.cpp
@@ -1,7 +1,7 @@
 /*
 * File: dish.cpp
 * Author: Ton Nu Minh Trang
-* Date: 15/02/2024
+* Date: 10/02/2024
 * Description: This file contains all the functions/methods to store dish's information
 */
 
