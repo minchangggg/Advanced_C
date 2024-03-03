75a755cfec4a8e837609dd20c053ac62d381f85b
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-03 15:05:04 +0700

    Update manager.h

--- Review 7: Worker Management/Code/Header/manager.h
+++ Review 7: Worker Management/Code/Header/manager.h
@@ -1,3 +1,10 @@
+/*
+* File: manager.h
+* Author: Ton Nu Minh Trang
+* Date: 15/02/2024
+* Description: This file is a Manager header file which helps hotel system work
+*/
+
 #ifndef _MANAGER_H
 #define _MANAGER_H
 
@@ -8,15 +15,28 @@
 typedef enum {
     Employee_function,
     Room_function,
-    Service_function
+    Service_function,
+    Back_function,
+    Exit_function
 } Function;
 
+/*
+* Struct: Database
+* Discription: This struct is used for containing data of hotel
+    employee: data of employees
+    room: data of rooms
+    service: data of services
+*/
 typedef struct {
     ManageEmployee employee;
     ManageRoom room;
     ManageService service;
 } Database;
 
+/*
+*Class: Manager
+*Description: This class contains basic properties and methods that help manager manages hotel system
+*/
 class Manager {
 private:
     int account;
