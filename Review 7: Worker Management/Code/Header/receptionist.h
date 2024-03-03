a3dbf639beb5cb29156b183644d2157498569410
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-04 00:17:21 +0700

    Update receptionist.h

--- Review 7: Worker Management/Code/Header/receptionist.h
+++ Review 7: Worker Management/Code/Header/receptionist.h
@@ -1,3 +1,10 @@
+/*
+* File: receptionist.h
+* Author: Ton Nu Minh Trang
+* Date: 15/02/2024
+* Description: This file is a Receptionist header file which helps hotel system work
+*/
+
 #ifndef _RECEPTIONIST_H
 #define _RECEPTIONIST_H
 
@@ -5,6 +12,10 @@
 
 using namespace std;
 
+/*
+*Class: Receptionist
+*Description: This class contains basic properties and methods that help receptionist manages hotel system
+*/
 class Receptionist  {
 private:
     int accountt;
