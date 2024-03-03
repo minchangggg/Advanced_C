7e0c7ecb6607d8a68933eb3893feff5532256ec9
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-04 03:32:53 +0700

    Update manager.h

--- Review 6: Restaurant Management/Code/Header/manager.h
+++ Review 6: Restaurant Management/Code/Header/manager.h
@@ -1,13 +1,24 @@
+/*
+* File: manager.h
+* Author: Ton Nu Minh Trang
+* Date: 10/02/2024
+* Description: This file is a table header file that program is used for manage restaurant
+*/
+
 #ifndef _MANAGER_H
 #define _MANAGER_H
 
 #include "table.h"
 using namespace std;
 
+/*
+* Class: Manager
+* Description: This class contains basic properties and methods of a Manager object
+*/
 class Manager {
 private:
     list <Dish> menu;
-    int numTable; // số lượng bàn set up
+    int numTable;
 
 public:
     Manager() {
@@ -15,21 +26,16 @@ public:
     }
 
     list <Dish> getMenu();
-    
-    // 1. Set up số bàn    
+ 
     void setNumTable (int _numTable);
     int setNumTable ();
 
-    // 2. Thêm món
     void addDish(); 
 
-    // 3. Sửa món
     void editDish(int ID_input); 
 
-    // 4. Xóa món
     void deleteDish(int ID_input); 
 
-    // 5. Danh sách món
     void showMenu(); 
 };
 
