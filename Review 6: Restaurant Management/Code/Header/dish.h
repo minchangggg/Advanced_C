7cfd337659a155bfdd3ceb761ea80e25984b9b66
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-04 03:08:31 +0700

    Update dish.h

--- Review 6: Restaurant Management/Code/Header/dish.h
+++ Review 6: Restaurant Management/Code/Header/dish.h
@@ -1,3 +1,10 @@
+/*
+* File: dish.h
+* Author: Ton Nu Minh Trang
+* Date: 10/02/2024
+* Description: This file is a dish header file that program is used for storing dish's information
+*/
+
 #ifndef _DISH_H
 #define _DISH_H
 
@@ -6,6 +13,10 @@
 
 using namespace std;
 
+/*
+* Class: Dish
+* Description: This class contains basic properties and methods of a Dish object
+*/
 class Dish {
 private:
     int ID;
