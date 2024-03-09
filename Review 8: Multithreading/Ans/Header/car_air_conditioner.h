47880fb9486f45b408082a2ff2a335b17bed2303
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-03-10 02:12:16 +0700

    Update car_air_conditioner.h

--- Review 8: Multithreading/Ans/Header/car_air_conditioner.h
+++ Review 8: Multithreading/Ans/Header/car_air_conditioner.h
@@ -1,35 +1,48 @@
 /*
-* File: car_air_conditioner
+* File: car_air_conditioner.h
 * Author: Ton Nu Minh Trang
 * Date: 03/03/2024
-* Description: This file is a Car Air Conditioner header file that program is used for control air conditioner
+* Description: This file is a Car Air Conditioner header file that program is used for controlling car air conditioner
 */
 
 #ifndef _CAR_AIR_CONDITIONER
 #define _CAR_AIR_CONDITIONER
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <thread>
 #include <condition_variable>
 #include <mutex>
+#include <shared_mutex> 
 #include <chrono>
 #include <future>
-#include <random>
+
+// hiển thị nhiệt độ trong xe
+// hiển thị nhiệt độ ngoài xe
+
+// hiển thị bảng điều chỉnh nhiệt độ trong xe
+// hiển thị bảng điều chỉnh mức gió trong xe
 
 using namespace std;
 
-mutex mtx_temp_outside, mtx_temp_inside, mtx_wind_insdide, mtx_data_inside, mtx_key_input;
-DeviceValue deviceValue;
+mutex mtx_temp_outside, mtx_temp_inside, mtx_wind_insdide, mtx_key_input;
 
-// Thông số giá trị điều hòa
+condition_variable cv_temp_outside, cv_temp_inside, cv_wind_insdide;
+
 typedef struct {
-    float temp_outside = 0;
-    float temp_inside = 0;
-    float temp_setting = 0;
+    DeviceMode deviceMode;
 
-    int sensor_wind = 0;
-    int wind_setting = 0;
+    int temp_outside;
+    int temp_inside;
+    int wind_inside;
+
+    HandControl wind_inside;
+    HandControl temp_setting;
+
 } DeviceValue;
+
+DeviceValue deviceValue = {ON, 20, 20, 1, ON, ON};
 
 // Chế độ điều hòa 
 typedef enum {
@@ -39,23 +52,14 @@ typedef enum {
 
 // Chế độ Bằng tay
 typedef enum {
-    ON,
     INCREASE,
-    DECREASE,
-    OFF
+    DECREASE
 } HandControl;
 
-// Các chế độ điều khiển của thiết bị 
-typedef struct {
-    float autoControl; // Chế độ Tự động
-    HandControl handControl; // Chế độ Bằng tay
-} DeviceControl;
-
-inline float getTempRandom() {
-    static random_device rd;
-    static mt19937 gen(rd());
-    static uniform_real_distribution<> dis(20.0, 40.0);
-    return dis(gen);
+// random từ 20 đến 40 độ
+inline int getTempRandom() { 
+    srand(time(NULL));  
+    return rand() % 21 + 3;
 }
 
 typedef struct {
@@ -63,18 +67,20 @@ typedef struct {
     void userMenu();
 
     // Task 2: Luồng sensor nhiệt độ bên ngoài xe
-    void setTempOutside();
-    float getTempOutside();
+    void setTempOutside(); // thiết lập temp outside
+    void getTempOutside(); // hiển thị temp lên màn hình
 
     // Task 3: Luồng sensor nhiệt độ bên trong xe
-    void setTempInside();
-    float getTempInside();
+    void setTempInside(); // thiết lập temp inside
+    void getTempInside(); // hiển thị temp lên màn hình
+
+    void getWindInside(); // hiển thị mức gió lên màn hình
 
     // Task 4: Luồng điều khiển điều hòa nhiệt độ
-    void controlTempInside();
+    void controlTempInside(HandControl handControl);
 
     // Task 5: Luồng điều khiển quạt gió (5 level; 0 là tắt quạt gió)
-    void controlWindInside();
+    void controlWindInside(HandControl handControl);
     
 } DeviceFunction;
 
